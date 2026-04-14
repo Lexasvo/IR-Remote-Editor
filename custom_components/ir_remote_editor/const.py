603fcf22fe31b6c336db678412417d8ee7ebaa81
Lexasvo <121602003+Lexasvo@users.noreply.github.com>
2026-04-14 10:52:23 +0900

    Create const.py

---
+++ custom_components/ir_remote_editor/const.py
@@ -0,0 +1,11 @@
+"""Constants for IR Remote Editor."""
+DOMAIN = "ir_remote_editor"
+NAME = "IR Remote Editor"
+MANUFACTURER = "IR Remote Editor"
+MODEL = "Virtual IR Remote"
+ICON = "mdi:remote"
+ICON_CONFIG = "mdi:remote"
+ICON_BUTTON_DEFAULT = "mdi:remote"
+
+# MQTT топики Tasmota
+MQTT_TOPIC_RESULT = "tele/{device}/RESULT"
